b6ce5ca13ad90cb1ba5aefce547c8843f88f802e
skpixels <makkobir79@gmail.com>
2023-04-24 22:15:44 +0600

    backend api created

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require("./data/categories.json");
-const allNews = require("./data/news.json");
+const news = require("./data/news.json");
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
@@ -17,7 +17,23 @@ app.get("/categories", (req, res) => {
 });
 
 app.get("/all-news", (req, res) => {
-  res.send(allNews);
+  res.send(news);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "0") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
