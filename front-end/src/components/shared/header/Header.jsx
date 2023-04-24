1c9d57d1103d2f900a36c65c4b89e4ad68f55492
skpixels <makkobir79@gmail.com>
2023-04-24 16:17:37 +0600

    moved to right

--- front-end/src/components/shared/header/Header.jsx
+++ front-end/src/components/shared/header/Header.jsx
@@ -10,23 +10,28 @@ export const Header = () => {
         <p>Sunday, November 27, 2022</p>
       </div>
       {/* banner  */}
-      <div className="bg-light d-flex p-2 align-items-center justify-content-center">
+      <div className="bg-light d-flex p-2 align-items-center justify-content-between">
         <Button variant="danger">Latest</Button>
-        <p className="ms-3 m-0">
+        <p className=" m-0 flex-grow-1 text-center">
           Match Highlights: Germany vs Spain — as it happened ! Match
           Highlights: Germany vs Spain as...
         </p>
       </div>
-      <nav className="mt-2">
-        <span className="ms-2" href="">
-          Home
-        </span>
-        <span className="ms-2" href="">
-          About
-        </span>
-        <span className="ms-2" href="">
-          Carrier
-        </span>
+      <nav className="mt-2 d-flex justify-content-between">
+        <div className="flex-grow-1 text-center">
+          <span className="ms-2" href="">
+            Home
+          </span>
+          <span className="ms-2" href="">
+            About
+          </span>
+          <span className="ms-2" href="">
+            Carrier
+          </span>
+        </div>
+        <div>
+          <Button variant="danger">Latest</Button>
+        </div>
       </nav>
     </header>
   );
